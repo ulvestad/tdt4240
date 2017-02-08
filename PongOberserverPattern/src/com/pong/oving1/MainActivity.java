74cb4cc8686f351d694a4e20974348fcb2b944d5
ulvestad <simenul@stud.ntnu.no>
2017-02-08 15:12:21 +0100

    oving2 Observer Pattern Task 2

---
+++ PongOberserverPattern/src/com/pong/oving1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.pong.oving1;
+
+import sheep.game.Game;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	 super.onCreate(savedInstanceState);
+
+         //create a new game using sheep documation
+         Game game = new Game(this, null);
+         //push gamestate onto gamestack
+         game.pushState(new GameStart());
+         //set the activity to the view
+         setContentView(game);
+
+
+    }
+
+
+}
